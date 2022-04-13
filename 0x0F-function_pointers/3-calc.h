fb7ff14b1faadbbee1f4e625ed68b0d78ab18ea1
dagfedlu <dagmawifedlu09@gmail.com>
2022-04-13 10:06:03 -0700

    fix

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,5 +1,6 @@
-#ifndef _HEADER_
-#define _HEADER_
+#ifndef CALC_H
+#define CALC_H
+
 /**
  * struct op - Struct op
  *
@@ -10,9 +11,7 @@ typedef struct op
 {
 char *op;
 int (*f)(int a, int b);
-}
-op_t;
-
+} op_t;
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
