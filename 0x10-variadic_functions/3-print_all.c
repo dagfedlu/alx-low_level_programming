d077e533601418e61a4402085f3a90f968969c73
dagfedlu <dagmawifedlu09@gmail.com>
2022-04-14 11:08:20 -0700

    fix 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -11,10 +11,10 @@
  */
 void t_char(va_list va)
 {
-  int c;
+int c;
 
-  c = va_arg(va, int);
-  printf("%c", c);
+c = va_arg(va, int);
+printf("%c", c);
 }
 
 /**
@@ -25,7 +25,7 @@ void t_char(va_list va)
  */
 void t_integer(va_list va)
 {
-  printf("%d", va_arg(va, int));
+printf("%d", va_arg(va, int));
 }
 
 /**
@@ -36,10 +36,10 @@ void t_integer(va_list va)
  */
 void t_float(va_list va)
 {
-  double c;
+double c;
 
-  c = va_arg(va, double);
-  printf("%f", c);
+c = va_arg(va, double);
+printf("%f", c);
 }
 /**
  * t_string - print a string
@@ -49,14 +49,14 @@ void t_float(va_list va)
  */
 void t_string(va_list va)
 {
-  char *s = va_arg(va, char *);
+char *s = va_arg(va, char *);
 
-  if (s == NULL)
-    {
-      printf("(nil)");
-      return;
-    }
-  printf("%s", s);
+if (s == NULL)
+{
+printf("(nil)");
+return;
+}
+printf("%s", s);
 }
 
 
@@ -68,36 +68,36 @@ void t_string(va_list va)
  */
 void print_all(const char * const format, ...)
 {
-  int i, j, count;
-  va_list valist;
-  types difftypes[] = {
-		       {'c', t_char},
-		       {'i', t_integer},
-		       {'f', t_float},
-		       {'s', t_string},
-  };
-  char *s = "";
+int i, j, count;
+va_list valist;
+types difftypes[] = {
+{'c', t_char},
+{'i', t_integer},
+{'f', t_float},
+{'s', t_string},
+};
+char *s = "";
 
-  i = 0;
-  count = 0;
-  va_start(valist, format);
-  while (format != NULL && format[i])
-    {
-      j = 0;
-      while (j < 4)
-	{
-	  if (format[i] == difftypes[j].t)
-	    {
-	      printf("%s", s);
-	      difftypes[j].f(valist);
-	      s = ", ";
-	      count++;
-	      break;
-	    }
-	  j++;
+i = 0;
+count = 0;
+va_start(valist, format);
+while (format != NULL && format[i])
+{
+j = 0;
+while (j < 4)
+{
+if (format[i] == difftypes[j].t)
+{
+printf("%s", s);
+difftypes[j].f(valist);
+s = ", ";
+count++;
+break;
+}
+j++;
 
-	}
-      i++;
-    }
-  printf("\n");
+}
+i++;
+}
+printf("\n");
 }
